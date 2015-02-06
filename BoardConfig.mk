db6dfecd964e55d7415d4021029c687de39247a4
Daniel Hillenbrand <daniel.hillenbrand@codeworkx.de>
2015-02-06 19:34:37 +0100

    build kernel from sources

--- BoardConfig.mk
+++ BoardConfig.mk
@@ -4,10 +4,8 @@ include device/huawei/h60-common/BoardConfigCommon.mk
 TARGET_SPECIFIC_HEADER_PATH += device/huawei/h60_l04/include
 
 # Kernel
-TARGET_PREBUILT_KERNEL := device/huawei/h60_l04/zImage
-# Inline kernel building
-#TARGET_KERNEL_CONFIG := cm_hi3630_defconfig
-#TARGET_KERNEL_SOURCE := kernel/huawei/h60
+TARGET_KERNEL_CONFIG := cm_hi3630_defconfig
+TARGET_KERNEL_SOURCE := kernel/huawei/h60
 
 # Bluetooth
 BOARD_BLUETOOTH_BDROID_BUILDCFG_INCLUDE_DIR := device/huawei/h60_l04/bluetooth

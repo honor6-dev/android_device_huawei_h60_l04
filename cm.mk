d6cfa3daded8c2ece14f9d4523993188fbd90746
Daniel Hillenbrand <codeworkx@cyanogenmod.org>
2015-10-03 08:11:58 +0100

    update fingerprint
    Change-Id: I4e2a39280a0219be04502129c0534ca3f6286b89

--- cm.mk
+++ cm.mk
@@ -6,7 +6,7 @@ $(call inherit-product, device/huawei/h60_l04/full_h60_l04.mk)
 # Inherit CM common Phone stuff.
 $(call inherit-product, vendor/cm/config/common_full_phone.mk)
 
-PRODUCT_BUILD_PROP_OVERRIDES += PRODUCT_NAME=H60-L04 BUILD_FINGERPRINT=Huawei/H60-L04/hwH60:4.4.2/HDH60-L04/CHNC00B310:user/ota-rel-keys,release-keys PRIVATE_BUILD_DESC="hi3630-user 4.4.2 KOT49H eng.jslave.20141208.235421 test-keys"
+PRODUCT_BUILD_PROP_OVERRIDES += PRODUCT_NAME=H60-L04 BUILD_FINGERPRINT=Huawei/H60-L04/hwH60:5.1.1/HDH60-L04/C900B521:user/release-keys PRIVATE_BUILD_DESC="H60-L04-user 5.1.1 HuaweiH60-L04 C900B521 release-keys"
 
 PRODUCT_NAME := cm_h60_l04
 PRODUCT_DEVICE := h60_l04
